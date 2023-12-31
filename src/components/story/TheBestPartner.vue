fda13481d7dfe3f7f89c2cc908a03dc78e1e3719
SimonMeia <simon@meia.dev>
2023-12-31 14:28:02 +0100

    best partner donut

--- src/components/story/TheBestPartner.vue
+++ src/components/story/TheBestPartner.vue
@@ -1,48 +1,46 @@
 <script setup lang="ts">
-import { VisXYContainer, VisGroupedBar, VisAxis } from '@unovis/vue'
-import { ref, watchEffect } from 'vue'
+import { VisSingleContainer, VisNestedDonut } from '@unovis/vue'
+import { computed } from 'vue'
 
 const props = defineProps<{
   topPartners: { name: string; numberOfMovies: number }[]
 }>()
 
-type DataRecord = { numberOfMoviesWatched: number; partnerId: number; partnerName: string }
-const data = ref<DataRecord[]>([])
+const numberOfPartners = computed(() => props.topPartners.length)
 
-const numberOfPartnerToDisplay = 3
-
-watchEffect(() => {
-  let counter = 0
-  data.value = props.topPartners.map((p) => ({
-    numberOfMoviesWatched: p.numberOfMovies,
-    partnerId: counter++,
-    partnerName: p.name
-  }))
-  data.value.splice(numberOfPartnerToDisplay)
+const data = computed(() => {
+  const array = []
+  for (const p of props.topPartners) {
+    for (let i = 0; i < p.numberOfMovies; i++) {
+      array.push(p.name)
+    }
+  }
+  return array
 })
 
-const x = (data: DataRecord) => data.partnerId
-const y = (data: DataRecord) => data.numberOfMoviesWatched
-
-function getPartnerName(id: number) {
-  return data.value.find((p) => p.partnerId === id)?.partnerName
-}
+const layers = [(d: string) => d]
 </script>
 
 <template>
   <div class="">
-    <h2 class="text-xl font-bold">Viewings by month</h2>
-    <VisXYContainer :data="data">
-      <VisGroupedBar :x="x" :y="y" />
-      <VisAxis type="y" :domainLine="false" />
-      <VisAxis
-        type="x"
-        :domainLine="false"
-        :gridLine="false"
-        :numTicks="2"
-        :tickFormat="(x: number) => getPartnerName(x)"
-      />
-    </VisXYContainer>
+    <p class="text-sm text-center">
+      you watched movies with <span class="text-xl font-bold">{{ numberOfPartners }}</span> partners
+    </p>
+    <VisSingleContainer :data="data" class="my-8">
+      <VisNestedDonut :layers="layers" />
+    </VisSingleContainer>
+    <div class="text-sm text-center">
+      <p>
+        your favourite partner is
+        <span class="text-xl font-bold">{{ topPartners[0].name }}</span> with
+        <span class="text-xl font-bold">{{ topPartners[0].numberOfMovies }}</span> movies
+      </p>
+      <p>
+        followed by
+        <span class="text-xl font-bold">{{ topPartners[1].name }}</span> with
+        <span class="text-xl font-bold">{{ topPartners[1].numberOfMovies }}</span> movies
+      </p>
+    </div>
   </div>
 </template>
 
